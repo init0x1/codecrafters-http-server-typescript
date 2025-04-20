22b82bf1e257249f5f2f7ea420fbaa2e727af437
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-04-20 18:26:00 +0200

    response with http version 1.1 200 ok

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,7 @@ console.log("Logs from your program will appear here!");
 
 
 const server = net.createServer((socket: net.Socket) => {
+socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
   socket.on("close", () => {
     socket.end();
   });
